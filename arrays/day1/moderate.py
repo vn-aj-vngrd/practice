601a5e0c547319ee7179b55e195d08dd55b0f418
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-01-15 07:34:47 +0800

    Fix duplicate detection in dup function

--- arrays/day1/moderate.py
+++ arrays/day1/moderate.py
@@ -7,5 +7,7 @@ def dup(arr):
     for i in range(len(arr)):
         for j in range(i + 1 ,len(arr)):
             if (arr[i] == arr[j]):
-                return True
+                return arr[i]
     return False
+
+print(dup([1, 2, 2, 3, 4, 5]))
